61741ddaaf784bb78bde9f6d0c6a047f072c59bc
kmiecik013 <kmiecik013@gmail.com>
2022-02-07 03:07:57 +0000

    added a an admin/category page to Jungle

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  get 'about', to: 'about#show'
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
-  get 'about', to: 'about#show'
 
   resource :cart, only: [:show] do
     post   :add_item
@@ -17,7 +18,9 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]  
   end
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -73,4 +76,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
